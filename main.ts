f954b2eb2f4b4caa515339dc3ad7d4035bb1d455
pgallowaaaaay <70017227+pgallowaaaaay@users.noreply.github.com>
2020-09-01 09:50:54 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,68 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    mySprite3.setPosition(randint(10, 150), randint(10, 110))
+    info.startCountdown(7)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.setScore(0)
+    game.reset()
+})
+let mySprite3: Sprite = null
+scene.setBackgroundColor(13)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 8 . . . . . . . . 
+    . . . . . . 8 8 8 . . . . . . . 
+    . . . . . 8 8 . 8 8 . . . . . . 
+    . . . . 8 8 . . . 8 8 . . . . . 
+    . . . 8 8 . 8 . 8 . 8 8 . . . . 
+    . . . 8 . . . . . . . 8 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
+    . . . . . . 8 . 8 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 55, 55)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 . . . . . . . 
+    . . . 2 2 1 1 1 2 2 . . . . . . 
+    . . . 2 1 2 1 2 1 2 . . . . . . 
+    . . . 2 1 1 1 1 1 2 . . . . . . 
+    . . . 2 1 2 2 2 1 2 . . . . . . 
+    . . . 2 2 1 1 1 2 2 . . . . . . 
+    . . . . 2 2 2 2 2 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+mySprite2.setPosition(135, 10)
+mySprite2.follow(mySprite, 30.5)
+mySprite3 = sprites.create(img`
+    4 4 4 . . 4 4 4 4 4 . . . . . . 
+    4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
+    b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
+    . b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
+    . b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
+    b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
+    c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+    c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
+    c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
+    c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
+    . c 4 5 5 5 5 d d d 5 5 5 5 5 b 
+    . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
+    . . c 4 4 d 4 4 4 4 4 d d 5 d c 
+    . . . c 4 4 4 4 4 4 4 4 5 5 5 4 
+    . . . . c c b 4 4 4 b b 4 5 4 4 
+    . . . . . . c c c c c c b b 4 . 
+    `, SpriteKind.Food)
